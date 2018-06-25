fe5c6ecc611cdb5070c2b448c25c647f37d06cce
shanghai-Jerry <cjyou20110920sspu@163.com>
2018-06-25 13:03:34 +0800

    zero lease uid

--- src/main/java/com/higgs/client/dgrpah/ZeroClient.java
+++ src/main/java/com/higgs/client/dgrpah/ZeroClient.java
@@ -41,24 +41,32 @@ public class ZeroClient {
     this.zeroClient = zeroClient;
   }
 
+
+  public void getStartId() {
+    DgraphProto.AssignedIds assigned;
+    assigned = zeroClient.timestamps(Internal.Num.newBuilder().setVal(1L).build());
+    logger.info("timestamp zero assigned ids:" + assigned.getStartId() + " - " + assigned.getEndId());
+  }
+
   /**
    * this operation can free uid lease in dgraph.
    * To avoid this kind of exception:
    * Exception:
    * Uid: [24387] cannot be greater than lease: [0]
+   * 获取已使用的num个uid区间, 每次都是申请num个uid（uid上限在每次申请的时候依次增加num）
+   * 这个区间内的下一个uid就是新增数据分配的uid
+   * 所以在初始化的时候需要找到已分配的最大uid，设置num为该最大uid值就可
+   * @param num  uid的个数
    */
-  public void freeLeaseUidInZero() {
-    DgraphProto.AssignedIds assigned = zeroClient.assignUids(Internal.Num.newBuilder().setVal(Long
-        .MAX_VALUE).build());
+  public void freeLeaseUidInZero(long num) {
+    DgraphProto.AssignedIds assigned;
+    assigned = zeroClient.assignUids(Internal.Num.newBuilder().setVal(num).build());
     logger.info("zero assigned ids:" + assigned.getStartId() + " - " + assigned.getEndId());
+
   }
 
   public static void main(final String[] args) {
      ZeroClient client = new ZeroClient("172.20.0.14:5080");
-     DgraphProto.AssignedIds assigned = client.getZeroClient().assignUids(Internal.Num.newBuilder()
-        .setVal(Long.MAX_VALUE).build());
-      logger.info("zero assigned ids:" + assigned.getStartId() + " - " + assigned.getEndId());
-
-
+     client.freeLeaseUidInZero(1000);
   }
 }
