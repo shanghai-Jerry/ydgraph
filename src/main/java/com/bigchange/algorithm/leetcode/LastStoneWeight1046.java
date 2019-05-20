25545d32f77763b69e83835db6c722b59241d308
shanghai-Jerry <cjyou20110920sspu@163.com>
2019-05-20 18:34:15 +0800

    fix

---
+++ src/main/java/com/bigchange/algorithm/leetcode/LastStoneWeight1046.java
@@ -0,0 +1,69 @@
+package com.bigchange.algorithm.leetcode;
+
+/**
+ * User: JerryYou
+ *
+ * Date: 2019-05-20
+ *
+ * Copyright (c) 2018 devops
+ *
+ * <<licensetext>>
+ */
+public class LastStoneWeight1046 {
+
+  class Find {
+    int value = 0;
+    int index = -1;
+    Find(int v, int i) {
+      value = v;
+      index = i;
+    }
+  }
+
+  public Find findMaxValue(int [] stones, int []visit) {
+    int maxValue = 0;
+    int index = -1;
+    for (int i = 0; i < stones.length; i++) {
+      if (visit[i] == 1) {
+          continue;
+      }
+      if (stones[i] > maxValue) {
+        maxValue = stones[i];
+        index = i;
+      }
+    }
+    return new Find(maxValue, index);
+  }
+  public int lastStoneWeight(int[] stones) {
+    int [] visit = new int[stones.length];
+    while (true) {
+      Find max = findMaxValue(stones, visit);
+      if (max.index == -1) {
+        return 0;
+      }
+      visit[max.index] = 1;
+
+      Find sec_max = findMaxValue(stones, visit);
+      if (sec_max.index == -1) {
+        return max.value;
+      }
+
+      if (max.value == sec_max.value) {
+        visit[max.index] = 1;
+        visit[sec_max.index] = 1;
+        stones[max.index] = 0;
+        stones[sec_max.index] = 0;
+      } else if (max.value > sec_max.value) {
+        visit[max.index] = 0;
+        visit[sec_max.index] = 1;
+        stones[max.index] = max.value - sec_max.value;
+        stones[sec_max.index] = 0;
+      } else {
+        visit[max.index] = 1;
+        visit[sec_max.index] = 0;
+        stones[max.index] = 0;
+        stones[sec_max.index] = max.value - sec_max.value;;
+      }
+    }
+  }
+}
---
+++ src/main/java/com/bigchange/algorithm/leetcode/RemoveAllAdjacentDuplicatesInString1047.java
@@ -0,0 +1,47 @@
+package com.bigchange.algorithm.leetcode;
+
+import java.util.ArrayList;
+
+/**
+ * User: JerryYou
+ *
+ * Date: 2019-05-20
+ *
+ * Copyright (c) 2018 devops
+ *
+ * <<licensetext>>
+ */
+public class RemoveAllAdjacentDuplicatesInString1047 {
+
+  // good solution
+  public String removeDuplicates_2(String S) {
+    if(S == null || S.length() < 2) return S;
+    int index = 0;
+    char[] chars = new char[S.length()];
+
+    for(char c : S.toCharArray()) {
+      if(index == 0 || chars[index - 1] != c) {
+        chars[index++] = c;
+      }
+      else {
+        index--;
+      }
+    }
+
+    return new String(chars).substring(0, index);
+  }
+
+
+  public String removeDuplicates_1(String S) {
+    // StringBuilder is not fast enough
+    StringBuilder sb = new StringBuilder();
+    for(char c : S.toCharArray()) {
+      if(sb.length() == 0 || sb.charAt(sb.length()-1) != c)
+        sb.append(c);
+      else
+        sb = sb.deleteCharAt(sb.length()-1);
+    }
+    return sb.toString();
+  }
+
+}
